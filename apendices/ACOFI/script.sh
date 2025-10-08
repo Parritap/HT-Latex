0028db2606a3a6e8626fc4c9c43dfa000ab5a859
Parritap <juane.parrap@uqvirtual.edu.co>
2025-10-08 10:33:07 -0500

    Eliminados apendices

--- apendices/ACOFI/script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-i=1
-# listamos en orden cronológico y leemos línea completa como nombre de archivo
-ls -tr Screenshot*.png | while IFS= read -r file; do
-  mv "$file" "pagina_$i.png"
-  ((i++))
-done
